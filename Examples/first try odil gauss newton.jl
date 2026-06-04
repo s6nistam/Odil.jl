ab4274fb22cfb1611b5958e37c3be14f61221786
s6nistam <nico.stamos@alumni.fh-aachen.de>
2026-06-04 20:14:30 +0200

    working gauß Newton odil

---
+++ Examples/first try odil gauss newton.jl
@@ -0,0 +1,72 @@
+using Odil
+import Pkg
+Pkg.add("SciMLBase")
+Pkg.add("NonlinearSolveFirstOrder")
+Pkg.add("ADTypes")
+Pkg.add("Enzyme")
+include("../src/FD/wave.jl")
+include("../src/solvers/odil_gauss_newton.jl")
+
+Odil.greet()
+
+Nx = 32
+Nt = 32
+
+x = range(0, 1, length=Nx)
+t = range(-.5, .5, length=Nt)
+tspan = (t[1], t[end])
+
+dx = x[2] - x[1]
+dt = t[2] - t[1]
+
+@info "dx ", dx
+@info "dt ", dt
+
+u_exact  = [get_exact_wave(xi, ti) for xi in x, ti in t]
+
+# u_fd = zero(u_exact)
+
+# # initial condition
+# u_fd[:, 1] .= u_exact[:, 1]
+# u_fd[:, 2] .= u_exact[:, 2]
+
+# # boundary conditions
+# u_fd[1, :] .= u_exact[1, :]
+# u_fd[end, :] .= u_exact[end, :]
+
+# solve_d2dt2_central!(u_fd, rhs_wave, dx, dt)
+f = ODEFunction{true}(rhs!)
+ode = ODEProblem{true}(f, u_exact[:, 1], tspan, (dx, x, t))
+
+function lhs!(du, u, p, it)
+    dt, x_array, t_array = p 
+    fill!(du, 0.0)
+    Nx = length(x_array)
+    Nt = length(t_array)
+    
+    if it == 1
+        t0 = t_array[1]
+        penalty_vel = Nt 
+        
+        for j in 2:Nx-1
+            v0 = get_exact_wave_velocity(x_array[j], t0)
+            du[j] = penalty_vel * 2 * (u[j, 2] - u[j, 1] - dt * v0) / dt^2
+        end
+    elseif it < size(u, ndims(u))
+        for j in 2:Nx-1
+            du[j] = (u[j, it+1] - 2 * u[j, it] + u[j, it-1]) / dt^2
+        end
+    end
+
+    penalty_bc = Nx
+    du[1]  = penalty_bc * u[1, it]
+    du[Nx] = penalty_bc * u[Nx, it]
+    
+    return nothing
+end
+
+u = odil_gauss_newton(ode, lhs!, (dt, x, t), Nt)
+# u_opt_matrix = reshape(res.u, Nx, Nt)
+
+plot_comparison(x, t, u_exact, u)
+
--- src/solvers/odil_gauss_newton.jl
+++ src/solvers/odil_gauss_newton.jl
@@ -1,20 +1,83 @@
-using SciMLBase, OptimizationBase, OptimizationLBFGSB, ADTypes, Zygote
+using SciMLBase, NonlinearSolveFirstOrder, ADTypes, Enzyme, LinearAlgebra
 
-function odil_gauss_newton(ode:ODEProblem, lhs, p_lhs)
+function odil_gauss_newton(ode::ODEProblem, lhs_func, p_lhs, Nt = 1000)
     u_t0 = ode.u0
-    u_iter0 = zeros(size(u_t0), length(ode.tspan))
-    du_rhs = zeros(size(u_t0), length(ode.tspan))
-    du_lhs = zeros(size(u_t0), length(ode.tspan))
-    function loss(ut, p)
-        p_rhs, p_lhs = p
-        u, t = ut
-        l_init = u[:, :, end] - u_t0
-        l_inner_boundary = ode.f(du_rhs, u, t, p_rhs) - lhs(du_lhs, u, t, p_lhs)
-        l = norm(l_init)^2 + norm(l_inner_boundary)^2
+    
+    space_dims = size(u_t0)
+    num_cells = prod(space_dims)
+    # num_unknowns = num_cells * (Nt - 1)
+    num_unknowns = num_cells * Nt
+    
+    u_iter0 = zeros(eltype(u_t0), num_unknowns)
+    
+    rhs_func = ode.f.f
+
+    p_all = (ode.p, p_lhs, u_t0)
+
+    function operator_loss(u_vec, p)
+        p_rhs, p_lhs_inner, u_t0_inner = p
+        
+        u_local = zeros(eltype(u_vec), space_dims..., Nt)
+        
+        for i in 1:length(u_vec)
+            # u_local[num_cells + i] = u_vec[i]
+            u_local[i] = u_vec[i]
+        end
+        
+        l_init = zeros(eltype(u_vec), space_dims..., Nt)
+
+        for i in 1:length(u_t0_inner)
+            l_init[i] += Nt *(u_local[i] - u_t0_inner[i])
+        end
+
+        l_inner = zeros(eltype(u_vec), space_dims..., Nt)
+        
+        du_rhs = similar(u_local)
+        du_lhs = similar(u_local)
+        
+        for it in 1:Nt
+            fill!(du_rhs, 0.0)
+            fill!(du_lhs, 0.0)
+            
+            rhs_func(du_rhs, u_local, p_rhs, it)
+            lhs_func(du_lhs, u_local, p_lhs_inner, it)
+            
+            for i in 1:num_cells
+                l_inner[(it - 1) * num_cells + i] += (du_rhs[i] - du_lhs[i])
+            end
+        end
+
+        l = l_init + l_inner
+
         return l
     end
-    prob = OptimizationProblem(loss, u_iter0, (ode.p, p_lhs))
-    opt = LBFGSB()
-    res = optimize(prob, opt)
-    return res
+    prob = NonlinearLeastSquaresProblem(operator_loss, u_iter0, p_all)
+    opt = GaussNewton(autodiff = AutoEnzyme())
+
+    counter = 0
+    callback = function (state, l)
+        counter += 1
+        if counter % 1000 == 0 || counter == 1
+            println("Iteration ", counter, ": Loss = ", norm(l))
+
+            u_current = zeros(eltype(u_t0), space_dims..., Nt)
+            for i in 1:length(state.u)
+                u_current[i] = state.u[i]
+            end
+            plot_comparison(x, t, u_exact, u_current)
+        end
+        return false # false = Optimierung weiterlaufen lassen
+    end
+    
+    println("Starte Lösung...")
+    res = solve(prob, opt, maxiters = 10000000, callback = callback)
+    
+    # Ergebnis für den Output wieder rekonstruieren
+    u_final = zeros(eltype(u_t0), space_dims..., Nt)
+    for i in 1:length(res.u)
+        u_final[i] = res.u[i]
+    end
+    println("Optimierung beendet!")
+    println("Return Code: ", res.retcode)
+    return u_final
 end
