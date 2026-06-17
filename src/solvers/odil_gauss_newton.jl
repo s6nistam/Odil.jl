bf9903f29b43df4637e3fa8a5721cfed49b5a1ad
s6nistam <nico.stamos@alumni.fh-aachen.de>
2026-06-17 19:19:00 +0200

    odil methods now work with just vectors

--- src/solvers/odil_gauss_newton.jl
+++ src/solvers/odil_gauss_newton.jl
@@ -1,55 +1,49 @@
 using SciMLBase, NonlinearSolveFirstOrder, ADTypes, Enzyme, LinearAlgebra
 
-function odil_gauss_newton(lhs, rhs, p_lhs, p_rhs, u_iter0_size, u_fixed_vals, x_fixed_indicies, t_fixed_indicies, t; max_iterations = 2, extra = nothing,  p_extra = nothing, u_iter0 = nothing, autodiff = AutoEnzyme())
-    space_dims = u_iter0_size
-    num_cells = prod(space_dims)
+function odil_gauss_newton(lhs, rhs, p_lhs, p_rhs, Nx, u_reference_vals, reference_val_indices, t; max_iterations = 2, extra = nothing,  p_extra = nothing, u_iter0 = nothing, autodiff = AutoEnzyme())
     Nt = length(t)
-    num_unknowns = num_cells * Nt
     p_iter = Ref(0)
     if u_iter0 === nothing
-        u_iter0 = zeros(num_unknowns)
+        u_iter0 = zeros(Nx * Nt)
     end
 
-    p_all = (lhs, rhs, extra, p_lhs, p_rhs, p_extra, u_fixed_vals, x_fixed_indicies, t_fixed_indicies, space_dims, num_cells, num_unknowns, t, Nt, p_iter)
+    p_all = (lhs, rhs, extra, p_lhs, p_rhs, p_extra, u_reference_vals, reference_val_indices, Nx, t, Nt, p_iter)
 
     function operator_loss(u_vec, p)
-        lhs_inner, rhs_inner, extra_inner, p_lhs_inner, p_rhs_inner, p_extra_inner, u_fixed_vals_inner, x_fixed_indicies_inner, t_fixed_indicies_inner, space_dims_inner, num_cells_inner, num_unknowns_inner, t_inner, Nt_inner, iter_inner = p
-
-        u_local = reshape(u_vec, space_dims_inner..., Nt_inner)
+        lhs_inner, rhs_inner, extra_inner, p_lhs_inner, p_rhs_inner, p_extra_inner, u_reference_vals_inner, reference_val_indices_inner, Nx_inner, t_inner, Nt_inner, iter_inner = p
         
-        l_exact = zeros(eltype(u_vec), length(u_fixed_vals_inner))
+        l_exact = zeros(eltype(u_vec), length(u_reference_vals_inner))
 
-        for (i, (u_val, ix, it)) in enumerate(zip(u_fixed_vals_inner, x_fixed_indicies_inner, t_fixed_indicies_inner))
-            #TODO: fix indicies. probably just use u_vec and use LinearIndices[...] to get the right index
-            l_exact[i] = (num_unknowns_inner/length(u_fixed_vals_inner)) * (u_local[ix..., it] - u_val)
+        for (i, (u_val, idx)) in enumerate(zip(u_reference_vals_inner, reference_val_indices_inner))
+            l_exact[i] = (u_vec[idx] - u_val)/length(u_reference_vals_inner)
         end
 
-        l_pde = zeros(eltype(u_vec), num_cells_inner * (Nt_inner - 1))
+        l_pde = zeros(eltype(u_vec), Nx_inner * (Nt_inner - 1))
         
-        du_rhs = zeros(eltype(u_vec), num_cells_inner)
-        du_lhs = zeros(eltype(u_vec), num_cells_inner)
+        du_rhs = zeros(eltype(u_vec), Nx_inner)
+        du_lhs = zeros(eltype(u_vec), Nx_inner)
         
         for it in 1:(Nt_inner - 1)
             t_val = t_inner[it]
             t_val_next = t_inner[it + 1]
-            u_rhs = vec(selectdim(u_local, length(space_dims_inner) + 1, it))
+            u_rhs = u_vec[((it - 1) * Nx_inner + 1):(it * Nx_inner)]
             fill!(du_rhs, 0.0)
             fill!(du_lhs, 0.0)
             
             rhs_inner(du_rhs, u_rhs, p_rhs_inner, t_val_next)
-            lhs_inner(du_lhs, u_local, p_lhs_inner, it)
+            lhs_inner(du_lhs, u_vec, p_lhs_inner, it)
             
-            for i in 1:num_cells_inner
-                l_pde[(it - 1) * num_cells_inner + i] = (du_rhs[i] - du_lhs[i])
+            for i in 1:Nx_inner
+                l_pde[(it - 1) * Nx_inner + i] = (du_rhs[i] - du_lhs[i])/Nx_inner
             end
         end
 
         l = vcat(l_exact, l_pde)
 
-        l_extra = zeros(eltype(u_vec), space_dims_inner..., Nt_inner)
+        l_extra = zeros(eltype(u_vec), Nx_inner, Nt_inner)
 
         if extra_inner !== nothing && p_extra_inner !== nothing
-            l_extra = extra_inner(u_local, p_extra_inner, iter_inner[])
+            l_extra = extra_inner(u_vec, p_extra_inner, iter_inner[])
             l = vcat(l, vec(l_extra))
         end
 
@@ -62,7 +56,7 @@ function odil_gauss_newton(lhs, rhs, p_lhs, p_rhs, u_iter0_size, u_fixed_vals, x
 
     callback = function (cache, iter)
         println("Iteration ", iter, ": Loss = ", norm(cache.fu))
-        u_current = reshape(cache.u, space_dims..., Nt)
+        u_current = reshape(cache.u, Nx, Nt)
         # plot_1d_time(x, t, u_current)
         return false # false = Optimierung weiterlaufen lassen
     end
@@ -83,7 +77,7 @@ function odil_gauss_newton(lhs, rhs, p_lhs, p_rhs, u_iter0_size, u_fixed_vals, x
     end
     
     # 3. Final reconstruction
-    u_final = reshape(cache.u, space_dims..., Nt)
+    u_final = reshape(cache.u, Nx, Nt)
     
     println("Optimierung beendet!")
     println("Return Code: ", cache.retcode)
--- src/solvers/odil_lbfgs.jl
+++ src/solvers/odil_lbfgs.jl
@@ -1,53 +1,49 @@
 using SciMLBase, OptimizationBase, OptimizationOptimJL, ADTypes, Enzyme, LinearAlgebra
 
-function odil_lbfgs(lhs, rhs, p_lhs, p_rhs, u_iter0_size, u_fixed_vals, x_fixed_indicies, t_fixed_indicies, t; extra = nothing,  p_extra = nothing, u_iter0 = nothing, max_iterations = 10000000, autodiff = AutoEnzyme())
-    space_dims = u_iter0_size
-    num_cells = prod(space_dims)
+function odil_lbfgs(lhs, rhs, p_lhs, p_rhs, Nx, u_reference_vals, reference_val_indices, t; extra = nothing,  p_extra = nothing, u_iter0 = nothing, max_iterations = 10000000, autodiff = AutoEnzyme())
     Nt = length(t)
-    num_unknowns = num_cells * Nt
+    num_unknowns = Nx * Nt
     iter = Ref(0)
     if u_iter0 === nothing
         u_iter0 = zeros(num_unknowns)
     end
 
-    p_all = (lhs, rhs, extra, p_lhs, p_rhs, p_extra, u_fixed_vals, x_fixed_indicies, t_fixed_indicies, space_dims, num_cells, num_unknowns, t, Nt, iter)
+    p_all = (lhs, rhs, extra, p_lhs, p_rhs, p_extra, u_reference_vals, reference_val_indices, Nx, t, Nt, iter)
 
     function loss(u_vec, p)
-        lhs_inner, rhs_inner, extra_inner, p_lhs_inner, p_rhs_inner, p_extra_inner, u_fixed_vals_inner, x_fixed_indicies_inner, t_fixed_indicies_inner, space_dims_inner, num_cells_inner, num_unknowns_inner, t_inner, Nt_inner, iter_inner = p
+        lhs_inner, rhs_inner, extra_inner, p_lhs_inner, p_rhs_inner, p_extra_inner, u_reference_vals_inner, reference_val_indices_inner, Nx_inner, t_inner, Nt_inner, iter_inner = p
         iter_inner[] += 1
-
-        u_local = reshape(u_vec, space_dims_inner..., Nt_inner)
         
         l_exact = zero(eltype(u_vec))
 
-        for (u_val, ix, it) in zip(u_fixed_vals_inner, x_fixed_indicies_inner, t_fixed_indicies_inner)
-            l_exact += (num_unknowns_inner/length(u_fixed_vals_inner))^2 *(u_local[ix..., it] - u_val)^2
+        for (u_val, idx) in zip(u_reference_vals_inner, reference_val_indices_inner)
+            l_exact += ((u_vec[idx] - u_val)/length(u_reference_vals_inner))^2
         end
 
         l_pde = zero(eltype(u_vec))
         
-        du_rhs = zeros(eltype(u_vec), num_cells_inner)
-        du_lhs = zeros(eltype(u_vec), num_cells_inner)
+        du_rhs = zeros(eltype(u_vec), Nx_inner)
+        du_lhs = zeros(eltype(u_vec), Nx_inner)
         
         for it in 1:(Nt_inner - 1)
-            u_rhs = vec(selectdim(u_local, length(space_dims_inner) + 1, it))
+            u_rhs = u_vec[((it - 1) * Nx_inner + 1):(it * Nx_inner)]
             fill!(du_rhs, 0.0)
             fill!(du_lhs, 0.0)
             
             t_val = t_inner[it]
             t_val_next = t_inner[it + 1]
             rhs_inner(du_rhs, u_rhs, p_rhs_inner, t_val_next)
-            lhs_inner(du_lhs, u_local, p_lhs_inner, it)
+            lhs_inner(du_lhs, u_vec, p_lhs_inner, it)
             
             for i in eachindex(du_rhs)
-                l_pde += (du_rhs[i] - du_lhs[i])^2
+                l_pde += ((du_rhs[i] - du_lhs[i])/Nx_inner)^2
             end
         end
 
         l_extra = zero(eltype(u_vec))
 
         if extra_inner !== nothing && p_extra_inner !== nothing
-            l_extra = extra_inner(u_local, p_extra_inner, iter_inner[])
+            l_extra = extra_inner(u_vec, p_extra_inner, iter_inner[])
             l_extra = sum(l_extra.^2)
         end
 
@@ -65,7 +61,7 @@ function odil_lbfgs(lhs, rhs, p_lhs, p_rhs, u_iter0_size, u_fixed_vals, x_fixed_
         if state.iter % 1000 == 0 || state.iter == 1
             println("Iteration ", state.iter, ": Loss = ", l)
 
-            u_current = reshape(state.u, space_dims..., Nt)
+            u_current = reshape(state.u, Nx, Nt)
             plot_1d_time(x, t, u_current)
         end
         return false
@@ -74,7 +70,7 @@ function odil_lbfgs(lhs, rhs, p_lhs, p_rhs, u_iter0_size, u_fixed_vals, x_fixed_
     println("Starte Lösung...")
     res = solve(prob, opt, maxiters = max_iterations, callback = callback)
     
-    u_final = reshape(res.u, space_dims..., Nt)
+    u_final = reshape(res.u, Nx, Nt)
     println("Optimierung beendet!")
     println("Return Code: ", res.retcode)
     return u_final
