df939deca4629e5bf3971a4dfa58f849491dba13
s6nistam <nico.stamos@alumni.fh-aachen.de>
2026-06-21 17:15:34 +0200

    made wave equation linearized

--- Examples/wave gauss newton.jl
+++ Examples/wave gauss newton.jl
@@ -12,6 +12,7 @@ dx = [x[i + 1] - x[i] for i in 1:Nx-1]
 dt = [t[i + 1] - t[i] for i in 1:Nt-1]
 
 u_exact = [get_exact_wave(x[ix], t[it]) for ix in 1:Nx, it in 1:Nt]
+u_t_exact = [get_exact_wave_velocity(x[ix], t[it]) for ix in 1:Nx, it in 1:Nt]
 
 p_lhs = (x, Nx, dx, t, Nt, dt)
 p_rhs = (x, Nx, dx, t, Nt, dt)
@@ -19,17 +20,26 @@ p_rhs = (x, Nx, dx, t, Nt, dt)
 u_t0  = [get_exact_wave(x[ix], t[1]) for ix in 1:Nx]
 u_bounds_left = [get_exact_wave(x[1], t[it]) for it in 2:Nt]
 u_bounds_right = [get_exact_wave(x[Nx], t[it]) for it in 2:Nt]
-u_reference_vals = [u_t0; u_bounds_left; u_bounds_right]
 
-idx = LinearIndices((Nx, Nt))
+u_t_t0  = [get_exact_wave_velocity(x[ix], t[1]) for ix in 1:Nx]
 
-idx_t0  = [idx[ix, 1] for ix in 1:Nx]
-idx_bounds_left = [idx[1, it] for it in 2:Nt]
-idx_bounds_right = [idx[Nx, it] for it in 2:Nt]
-reference_val_indices = [idx_t0; idx_bounds_left; idx_bounds_right]
+u_reference_vals = [u_t0; u_bounds_left; u_bounds_right; u_t_t0]
 
-u = odil_gauss_newton(lhs!, rhs!, p_lhs, p_rhs, Nx, u_reference_vals, reference_val_indices, t)
+idx = LinearIndices((Nx, 2, Nt))
 
-# plot_1d_time(x, t, u)
-plot_1d_time_comparison(x, t, u_exact, u)
+idx_t0  = [idx[ix, 1, 1] for ix in 1:Nx]
+idx_bounds_left = [idx[1, 1, it] for it in 2:Nt]
+idx_bounds_right = [idx[Nx, 1, it] for it in 2:Nt]
+
+idx_t_t0  = [idx[ix, 2, 1] for ix in 1:Nx]
+
+reference_val_indices = [idx_t0; idx_bounds_left; idx_bounds_right; idx_t_t0]
+
+u = odil_gauss_newton(lhs!, rhs!, p_lhs, p_rhs, 2 * Nx, u_reference_vals, reference_val_indices, t)
+
+u = reshape(u, Nx, 2, Nt)
+
+# plot_1d_time(x, t, u_exact)
+plot_1d_time_comparison(x, t, u_exact, u[:, 1, :])
+# plot_1d_time_comparison(x, t, u_t_exact, u[:, 2, :])
 
--- src/semidiscretization/wave.jl
+++ src/semidiscretization/wave.jl
@@ -1,53 +1,27 @@
 function rhs!(du, u, p, t_val)
     x, Nx, dx, t, Nt, dt = p 
     fill!(du, 0.0)
+    idx = LinearIndices((Nx, 2))
     
-    bc_left = get_exact_wave(x[1], t_val)
-    bc_right = get_exact_wave(x[Nx], t_val)
-    
-    if t_val == t[1]
-        du[2] = Nt * (bc_left - 2 * u[2] + u[3]) / (dx[1]^2)
-    
-        for ix in 3:Nx-2
-            du[ix] = Nt * (u[ix-1] - 2 * u[ix] + u[ix+1]) / (dx[ix]^2)
-        end
-        
-        du[Nx-1] = Nt * (u[Nx-2] - 2 * u[Nx-1] + bc_right) / (dx[Nx-1]^2)
-    else
-        du[2] = (bc_left - 2 * u[2] + u[3]) / (dx[1]^2)
-        
-        for ix in 3:Nx-2
-            du[ix] = (u[ix-1] - 2 * u[ix] + u[ix+1]) / (dx[ix]^2)
-        end
-    
-        du[Nx-1] = (u[Nx-2] - 2 * u[Nx-1] + bc_right) / (dx[Nx-1]^2)
+    du[idx[:, 1]] .= u[idx[:, 2]]
+    for ix in 2:Nx-1
+        du[idx[ix, 2]] = (u[idx[ix-1, 1]] - 2 * u[idx[ix, 1]] + u[idx[ix+1, 1]]) / (dx[ix]^2)
     end
-    
+
     return nothing
 end
 
 function lhs!(du, u, p, it)
     x, Nx, dx, t, Nt, dt = p 
-    fill!(du, 0.0)
-    idx = LinearIndices((Nx, Nt))
-    
-    if it == 1
-        t0 = t[1]
-        penalty_vel = Nt 
-        
-        for ix in 2:Nx-1
-            v0 = get_exact_wave_velocity(x[ix], t0)
-            du[ix] = penalty_vel * (u[idx[ix, 2]] - u[idx[ix, 1]] - dt[1] * v0) / dt[1]^2
-        end
-    elseif it < size(u, ndims(u))
-        for ix in 2:Nx-1
-            du[ix] = (u[idx[ix, it+1]] - 2 * u[idx[ix, it]] + u[idx[ix, it-1]]) / dt[it]^2
-        end
+    fill!(du, zero(eltype(u)))
+    idx = LinearIndices((Nx, 2, Nt))
+    idx_x = LinearIndices((Nx, 2))
+    for ix in 1:Nx
+        du[idx_x[ix, 1]] = (u[idx[ix, 1, it]] - u[idx[ix, 1, it - 1]]) / dt[it - 1]
     end
 
-    # penalty_bc = Nx/2
-    # du[1]  = penalty_bc * (u[idx[1, it + 1]] - get_exact_wave(x[1], t[it + 1]))
-    # du[idx[Nx, it + 1]] = penalty_bc * (u[idx[Nx, it + 1]] - get_exact_wave(x[Nx], t[it + 1]))
-    
+    for ix in 1:Nx
+        du[idx_x[ix, 2]] = (u[idx[ix, 2, it]] - u[idx[ix, 2, it - 1]]) / dt[it - 1]
+    end
     return nothing
 end
