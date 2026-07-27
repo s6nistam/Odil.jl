35ed641e73f68db87c4c3e77b61485043ce1b718
s6nistam <nico.stamos@alumni.fh-aachen.de>
2026-07-27 16:00:08 +0200

    added 3d euler example

---
+++ Examples/dgsem/3d/euler/3d_euler_sedov_blast_wave_gauss_newton.jl
@@ -0,0 +1,30 @@
+using Odil
+include("./dgsem_euler_sedov_blast_wave.jl")
+
+polydeg = 2
+refinement_level = 2
+ndims = 3
+variables = Int64(length(ode.u0)/((polydeg + 1)^ndims * (2^refinement_level)^ndims))
+
+coords = semi.cache.elements.node_coordinates
+x = coords[1, :, :, :, :]
+y = coords[2, :, :, :, :]
+z = coords[3, :, :, :, :]
+
+Nx = variables * length(x)
+t = sol.t
+Nt = length(t)
+dt = [t[i + 1] - t[i] for i in 1:Nt-1]
+
+u_exact = reduce(hcat, vec.(sol.u))
+
+timestep! = get_timestep(Odil.CarpenterKennedy2N54())
+p_timestep = (ode.f, ode.p)
+
+problem = OdilProblem(timestep!, p_timestep, Nx, ode.u0, 1:length(ode.u0), t, x, y, z ; timestep_alloc_size = 2 * Nx)
+res = odil_timestepping(problem, odil_gauss_newton, "odil_3d_euler_sedov_blast_wave_gauss_newton"; t_chunk_size = 4, max_iterations = 200)
+# res = odil_gauss_newton(problem; max_iterations = 10)
+
+plot(problem, u_exact, res)
+
+write_vtk(problem, res, "odil_3d_euler_sedov_blast_wave_gauss_newton")
