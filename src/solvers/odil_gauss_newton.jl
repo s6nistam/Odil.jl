02fdb9c55e57cd360336fdf2f3a1cf0777030cfe
s6nistam <nico.stamos@alumni.fh-aachen.de>
2026-07-29 08:40:11 +0200

    fixed some inconsistent variable naming

--- src/solvers/odil_gauss_newton.jl
+++ src/solvers/odil_gauss_newton.jl
@@ -3,19 +3,19 @@ using SciMLBase, NonlinearSolveFirstOrder, ADTypes, Enzyme, LinearAlgebra, Symbo
 function odil_gauss_newton(problem::OdilProblem; max_iterations = 2, timestep_alloc_size = problem.timestep_alloc_size, extra = problem.extra, p_extra = problem.p_extra, len_extra = problem.len_extra, u_iter0 = problem.u_iter0, autodiff = AutoEnzyme(), info_prints = true, jac_sparse = nothing, colors = nothing)
     return odil_gauss_newton(problem.timestep, problem.p_timestep, problem.N_coords, problem.u_reference_vals, problem.reference_val_indices, problem.t; max_iterations = max_iterations, timestep_alloc_size = timestep_alloc_size, extra = extra, p_extra = p_extra, len_extra = len_extra, u_iter0 = u_iter0, autodiff = autodiff, problem = problem, info_prints = info_prints, jac_sparse = jac_sparse, colors = colors)
 end
-function odil_gauss_newton(timestep, p_timestep, Nx, u_reference_vals, reference_val_indices, t; max_iterations = 2, timestep_alloc_size = 0, extra = nothing,  p_extra = nothing, len_extra = 0, u_iter0 = nothing, autodiff = AutoEnzyme(), problem = nothing, info_prints = true, jac_sparse = nothing, colors = nothing)
+function odil_gauss_newton(timestep, p_timestep, N_coords, u_reference_vals, reference_val_indices, t; max_iterations = 2, timestep_alloc_size = 0, extra = nothing,  p_extra = nothing, len_extra = 0, u_iter0 = nothing, autodiff = AutoEnzyme(), problem = nothing, info_prints = true, jac_sparse = nothing, colors = nothing)
     Nref = length(u_reference_vals)
     Nt = length(t)
     p_iter = Ref(0)
     if u_iter0 === nothing
-        u_iter0 = zeros(Nx * Nt)
+        u_iter0 = zeros(N_coords * Nt)
     end
 
-    p_all = (timestep, p_timestep, timestep_alloc_size, u_reference_vals, reference_val_indices, Nref, Nx, Nt, t, extra, p_extra, len_extra, p_iter)
-    resid_prototype = zeros(eltype(u_iter0), Nref + Nx * (Nt - 1) + len_extra)
+    p_all = (timestep, p_timestep, timestep_alloc_size, u_reference_vals, reference_val_indices, Nref, N_coords, Nt, t, extra, p_extra, len_extra, p_iter)
+    resid_prototype = zeros(eltype(u_iter0), Nref + N_coords * (Nt - 1) + len_extra)
 
     function operator_loss!(du, u_vec, p)
-        timestep_inner, p_timestep_inner, timestep_alloc_size_inner, u_reference_vals_inner, reference_val_indices_inner, Nref_inner, Nx_inner, Nt_inner, t_inner, extra_inner, p_extra_inner, len_extra_inner, iter_inner = p
+        timestep_inner, p_timestep_inner, timestep_alloc_size_inner, u_reference_vals_inner, reference_val_indices_inner, Nref_inner, N_coords_inner, Nt_inner, t_inner, extra_inner, p_extra_inner, len_extra_inner, iter_inner = p
         
         du[1:Nref_inner] .= zero(eltype(u_vec))
         l_exact = @view(du[1:Nref_inner])
@@ -26,22 +26,22 @@ function odil_gauss_newton(timestep, p_timestep, Nx, u_reference_vals, reference
             l_exact[i] = (u_vec[idx] - u_val)/sqrt(Nref_inner)
         end
 
-        du[Nref_inner + 1:Nref_inner + Nx_inner * (Nt_inner - 1)] .= zero(eltype(u_vec))
-        l_pde = @view(du[Nref_inner + 1:Nref_inner + Nx_inner * (Nt_inner - 1)])
+        du[Nref_inner + 1:Nref_inner + N_coords_inner * (Nt_inner - 1)] .= zero(eltype(u_vec))
+        l_pde = @view(du[Nref_inner + 1:Nref_inner + N_coords_inner * (Nt_inner - 1)])
 
-        u_timestep = zeros(eltype(u_vec), Nx_inner)
+        u_timestep = zeros(eltype(u_vec), N_coords_inner)
         timestep_mem = zeros(eltype(u_vec), timestep_alloc_size_inner)
         for it in 2:Nt_inner
             fill!(u_timestep, zero(eltype(u_vec)))
             fill!(timestep_mem, zero(eltype(u_vec)))
-            u_it_last = @view(u_vec[(it - 2) * Nx_inner + 1:(it - 1) * Nx_inner])
-            u_it = @view(u_vec[(it - 1) * Nx_inner + 1:it * Nx_inner])
+            u_it_last = @view(u_vec[(it - 2) * N_coords_inner + 1:(it - 1) * N_coords_inner])
+            u_it = @view(u_vec[(it - 1) * N_coords_inner + 1:it * N_coords_inner])
             timestep_inner(timestep_mem, u_timestep, u_it_last, t_inner[it - 1], t_inner[it] - t_inner[it - 1], p_timestep_inner)
-            l_pde[(it - 2) * Nx_inner + 1: (it - 1) * Nx_inner] .= (u_timestep - u_it)/sqrt(Nx_inner * Nt_inner)
+            l_pde[(it - 2) * N_coords_inner + 1: (it - 1) * N_coords_inner] .= (u_timestep - u_it)/sqrt(N_coords_inner * Nt_inner)
         end
 
-        du[Nref_inner + Nx_inner * (Nt_inner - 1) + 1:end] .= zero(eltype(u_vec))
-        l_extra = @view(du[Nref_inner + Nx_inner * (Nt_inner - 1) + 1:end])
+        du[Nref_inner + N_coords_inner * (Nt_inner - 1) + 1:end] .= zero(eltype(u_vec))
+        l_extra = @view(du[Nref_inner + N_coords_inner * (Nt_inner - 1) + 1:end])
 
         if extra_inner !== nothing && p_extra_inner !== nothing
             extra_inner(l_extra, u_vec, p_extra_inner, iter_inner[])
@@ -55,7 +55,7 @@ function odil_gauss_newton(timestep, p_timestep, Nx, u_reference_vals, reference
         if info_prints
             println("Computing Jacobian sparsity pattern...")
         end
-        jac_sparse = get_jac_sparse(timestep, p_timestep, timestep_alloc_size, t, Nref, Nx, Nt, reference_val_indices, extra, p_extra, len_extra, u_iter0)
+        jac_sparse = get_jac_sparse(timestep, p_timestep, timestep_alloc_size, t, Nref, N_coords, Nt, reference_val_indices, extra, p_extra, len_extra, u_iter0)
     end
 
     if colors === nothing
