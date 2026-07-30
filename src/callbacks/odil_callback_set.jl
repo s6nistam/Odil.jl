57b791ce034bb60ac1e2347e6dbf7ae0d75017d0
s6nistam <nico.stamos@alumni.fh-aachen.de>
2026-07-30 10:21:56 +0200

    fixed a bug where Callback Sets can only have one type of callback

--- src/callbacks/odil_callback_set.jl
+++ src/callbacks/odil_callback_set.jl
@@ -1,13 +1,15 @@
-mutable struct OdilCallbackSet{F}
-    callbacks::Vector{F}
+mutable struct OdilCallbackSet
+    callbacks::Vector{Function}
 end
 
-OdilCallbackSet(cbs...) = OdilCallbackSet(collect(cbs))
+OdilCallbackSet(cb::Function) = OdilCallbackSet(Function[cb])
+
+OdilCallbackSet(cbs...) = OdilCallbackSet(Function[cbs...])
 
 Base.iterate(cs::OdilCallbackSet, state...) = iterate(cs.callbacks, state...)
 Base.length(cs::OdilCallbackSet) = length(cs.callbacks)
 
-function add!(cs::OdilCallbackSet, cb::F) where {F}
+function add!(cs::OdilCallbackSet, cb::Function)
     if !(cb in cs.callbacks)
         push!(cs.callbacks, cb)
     end
